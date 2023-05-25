75b8b8df9148593d0047515a17588e51c59f6d65
Jose Jaime Ojeda Rodriguez <55614859+Ojedarjoe@users.noreply.github.com>
2023-05-25 15:29:17 -0700

    Create colorflash.py

---
+++ 034 - color flash/colorflash.py
@@ -0,0 +1,10 @@
+#Jose Jaime Ojeda Rodrigueznb 
+import machine
+
+sensor_pin = machine.Pin(2, machine.Pin.IN)  # Pin GP2
+
+while True:
+    if sensor_pin.value() == 0:
+        print("Línea detectada")
+    else:
+        print("No se detecta línea")
